fd4cb267da0683e9272d04f4c155e0f81cb6f6a0
Igor Vaynberg <igor.vaynberg@gmail.com>
2012-05-16 10:38:40 -0700

    release 2.0

--- select2.js
+++ select2.js
@@ -1,7 +1,7 @@
 ﻿/*
  Copyright 2012 Igor Vaynberg
  
- Version: @@ver@@ Timestamp: @@timestamp@@
+ Version: 2.0 Timestamp: Wed, May 16, 2012 10:38:37 AM
 
  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this work except in
  compliance with the License. You may obtain a copy of the License in the LICENSE file, or at:
